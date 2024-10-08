f27f93de12e245861adfb800e72c1292b91cc238
TeamSangai <teamsangaiwastaken@gmail.com>
2024-10-07 22:52:45 -0400

    a

--- src/main/java/mrgeoves/weatherascended/WeatherAscendedClient.java
+++ src/main/java/mrgeoves/weatherascended/WeatherAscendedClient.java
@@ -3,4 +3,8 @@ package mrgeoves.weatherascended;
 import net.fabricmc.api.ClientModInitializer;
 
 public class WeatherAscendedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
